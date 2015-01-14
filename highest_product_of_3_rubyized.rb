686722df4578b16b8bfc73eaee888da199fc0698
Chris Bastian <peacethrubeats@gmail.com>
2015-01-13 20:44:47 -0800

    updates

--- highest_product_of_3_rubyized.rb
+++ highest_product_of_3_rubyized.rb
@@ -1,21 +1,14 @@
 # Given an array_of_ints, find the highest_product you can get from three of the integers.
 # The input array_of_ints will always have at least three integers.
-def three_highest_ints(array)
-  for index in 0..array.length-1
-    nested_index = index + 1
-    for nested_index in 0..array.length-1
-      if array[index] > array[nested_index]
-        array[index], array[nested_index] = array[nested_index], array[index]
-      end
-    end
-  end
-  return array[0] * array[1] * array[2]
+def product_of_three(array)
+  sorted = array.sort.reverse.shift(3)
+  sorted.reduce(1, :*)
 end
 
 #_____________________ TEST CODE ____________________________
 
  array_of_ints = [12,6,44,1,98,45,22,78,983,989]
- p three_highest_ints(array_of_ints)
+ p product_of_three(array_of_ints)
 
 
 # While loop
