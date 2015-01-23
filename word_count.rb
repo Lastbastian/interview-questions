681671e7f21e7fd979578ca983d2de74534194b2
Chris Bastian <peacethrubeats@gmail.com>
2015-01-23 02:32:44 -0800

    word_count complete

--- word_count.rb
+++ word_count.rb
@@ -1,12 +1,9 @@
 # Using the Ruby language, have the function WordCount(str) take the str string parameter being passed and return the number of words the string contains (ie. "Never eat shredded wheat" would return 4). Words will be separated by single spaces.
 
-def word_count(str)
-  str_array = str.split("")
-  space_count = 0
-  str_array.each do |element|
-    space_count += 1 if element == " "
+def prime(num)
+  for i in (2..num-1)
+    return false if num % i != 0
   end
-  return space_count + 1
 end
 
-p word_count("lane borrowed")
+p prime(67)
